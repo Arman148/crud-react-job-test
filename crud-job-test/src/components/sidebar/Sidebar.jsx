52f03d23b0d25849a263d93c5973b84b6ef424c8
Arman Vardanyan <vardanyanarman148@yahoo.com>
2025-09-15 04:01:31 +0200

    Minor CSS imporvements

--- crud-job-test/src/components/sidebar/Sidebar.jsx
+++ crud-job-test/src/components/sidebar/Sidebar.jsx
@@ -12,7 +12,24 @@ const Sidebar = () => {
 
     return (
         <div className="sidebar">
-            <button onClick={goToAnnouncements}>Announcements</button>
+            <div>City Name</div>
+            <button onClick={goToAnnouncements}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="black"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    width="20"
+                    height="20"
+                >
+                    <path d="M3 10v4h2l5 5V5L5 10H3z" />
+                    <path d="M15 10h4v4h-4z" />
+                </svg>
+                Announcements
+            </button>
         </div>
     );
 }
