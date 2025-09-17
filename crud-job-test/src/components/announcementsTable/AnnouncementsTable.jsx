654216cf3ada5d1074238d3715bb63844fc2486e
Arman Vardanyan <vardanyanarman148@yahoo.com>
2025-09-17 02:19:34 +0200

    CSS Responsiveness and Color Improvements
    made huge changes for more responsive design, now table has horizontal scroll for better responsivity, now sidebar becomes header for small screens without bugs, also used figma extension to get the exact same colors for buttons as in the tasl

--- crud-job-test/src/components/announcementsTable/AnnouncementsTable.jsx
+++ crud-job-test/src/components/announcementsTable/AnnouncementsTable.jsx
@@ -119,39 +119,41 @@ const AnnouncementsTable = () => {
     }, []);
 
     return (
-        <table>
-            <thead>
-                {table.getHeaderGroups().map(headerGroup => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map(header => (
-                            <th
-                                key={header.id}
-                                onClick={header.column.getToggleSortingHandler()}
-                                style={{ cursor: "pointer" }}
-                            >
-                                {flexRender(header.column.columnDef.header, header.getContext())}
-                                {{
-                                    asc: " ▲",
-                                    desc: " ▼"
-                                }[header.column.getIsSorted()] ?? null}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
+        <div className="table-wrapper">
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th
+                                    key={header.id}
+                                    onClick={header.column.getToggleSortingHandler()}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    {flexRender(header.column.columnDef.header, header.getContext())}
+                                    {{
+                                        asc: " ▲",
+                                        desc: " ▼"
+                                    }[header.column.getIsSorted()] ?? null}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
 
-            <tbody>
-                {table.getRowModel().rows.map(row => (
-                    <tr key={row.id}>
-                        {row.getVisibleCells().map(cell => (
-                            <td key={cell.id}>
-                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
